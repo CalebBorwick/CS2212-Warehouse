255ae8ee25f1348d002f547e38c5f52c2f067d12
CalebBorwick <50631296+CalebBorwick@users.noreply.github.com>
2020-03-22 19:28:46 -0400

    Delete IndividualDefaultPricingStrategy.java

--- pricingStrategies/individual/IndividualDefaultPricingStrategy.java
+++
@@ -1,9 +0,0 @@
-package ca.uwo.pricingStrategies.individual;
-
-public class IndividualDefaultPricingStrategy implements IndividualPricingStrategy {
-
-	@Override
-	public double calculate(int quantity, double price) {
-		return quantity*price;
-	}
-}
