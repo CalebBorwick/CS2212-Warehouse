89be5ba2cad67d66f679ab683a4fbd6bb35da732
Caleb Borwick <50631296+CalebBorwick@users.noreply.github.com>
2020-06-23 16:22:26 -0700

    Add files via upload

---
+++ CS-2212-Final/src/Sorter.java
@@ -0,0 +1,13 @@
+
+public class Sorter {
+	private Strategy = null;
+	
+	Strategy strat = SortingAlgorithm AlgorithmSelector.select(SequenceType aSequenceType, int aSequenceSize));
+
+	Sorter.setStrategy(strat);
+	
+	public void setStrategy(Strategy strat){
+		this.strategy = strat;
+	}
+
+}
